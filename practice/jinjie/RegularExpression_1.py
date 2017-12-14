4f846fb85684e18a0ceb8a44c706ea139ddc2899
lizhuxian <lizhuxian020@pingan.com.cn>
2017-12-14 19:56:39 +0800

    =.=

--- practice/jinjie/RegularExpression_1.py
+++ practice/jinjie/RegularExpression_1.py
@@ -16,20 +16,52 @@ print match_obj.groups()
 
 # 2 字符串："one1two2three3four4" 用正则处理，输出 "1234"
 info_1 = 'one1two2three3four4'
-asd = re.match(r'[a-z]+([0-9])', info_1)
-print asd.groups()
-# split_obj = re.split(r'[a-z]{3,5}', info_1)
-# print ''.join([x for x in split_obj if len(x) > 0])
+# asd = re.match(r'[a-z]+([0-9])', info_1)
+# print asd.groups()
+split_obj = re.split(r'[a-z]+', info_1)
+print ''.join([x for x in split_obj if len(x) > 0])
+findall = re.findall(r'\d+', info_1)
+print findall
+p = re.compile(r'\d+')
+for m in p.finditer(info_1):
+    print m.group()
 #
 # 3 已知字符串：text = "JGood is a handsome boy, he is cool, clever, and so on..." 查找所有包含'oo'的单词。
-text = "JGood is a handsome boy, he is cool, clever, and so on..."
+text = "JGood is a handsome boy, he is cool> clever, and so onoo..."
 text_match = re.split(r' ', text)
 print [x for x in text_match if 'oo' in x]
+print re.findall(r'\w*oo\w*[ |,|.]', text)
 #
 # 4 为什么在unix里，grep后面的正则有些时候要加引号，有些时候不需要。
 # 已知字符串：
 #
-info_2 = 'test,&nbsp;url("http://www.baidu.com")&,dddddd "="" <svg></svg><path></path><img src="http://www.baidu.com">ininnnin<img src="http://www.dd.com">'
+info_2 = 'test,&nbsp;url("http://www.baidu.com")&,dddddd "="" <svg></svg><path></path><img src="http://www.baidu.com">ininnnin<img src="http://www.dd.com"'
+
+
+def _test(match_1):
+    print match_1.group()
+    return '\''
+
+
+def _test1(match_1):
+    print match_1.group() + '--'
+    return match_1.group()+'</img>'
+p = re.sub(r'"', _test, info_2)
+print p
+p = re.sub(r'<img.*>', _test1, p, 2)
+print p
+# p.sub()
+inputStr = "hello crifan, nihao crifan"
+replacedStr = re.sub(r"crifan", "crifanli", inputStr)
+print replacedStr
+print inputStr
+#
+#
+# inputStr = "hello crifan, nihao crifan";
+# replacedStr = re.sub(r"hello (\w+), nihao \1", "\g<1>", inputStr);
+# print "replacedStr=", replacedStr; #crifan
+# print inputStr
+
 #
 # 要求完成下面2个小功能：
 # 1.1 关闭[img]标签
@@ -63,3 +95,7 @@ print "p.pattern:", p.pattern
 print "p.flags:", p.flags
 print "p.groups:", p.groups
 print "p.groupindex:", p.groupindex
+
+pattern = re.compile(r'world')
+match = pattern.search('hello world')
+print match.group()
